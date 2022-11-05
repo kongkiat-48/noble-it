5e47a8f45e1c9adf6174ff65441b223713ef8bab
kongkiat96 <kongkiat.0174@gmail.com>
2022-11-05 13:54:43 +0700

    masterCode

---
+++ core/config.core.php
@@ -0,0 +1,12 @@
+<?php
+// MySQL hostname
+define('DB_HOST', 'localhost:3306');
+
+// MySQL database username
+define('DB_USERNAME', 'root');
+
+// MySQL database password
+define('DB_PASSWORD', '');
+
+// MySQL database name
+define('DB_NAME', 'keep_nb_it');
